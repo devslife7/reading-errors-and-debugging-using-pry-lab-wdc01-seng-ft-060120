a8391f72795559252d710602068c95d627b08d82
Marcos Velasco <devslife7@gmail.com>
2020-05-19 23:16:20 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,12 +12,14 @@ def prompt_user
 end
 
 def selection(num)
+  message = ""
   binding.pry
   if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
+     message = "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+     message = "HAM HAM HAM IN MY TUMMY"
   end
+message
 end
 
 def runner
